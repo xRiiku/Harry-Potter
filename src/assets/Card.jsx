b628e9a51448282f42b50c0374e43950b7a1f172
Daniel Ramon Campos <drc89.sxm@gmail.com>
2024-01-06 17:58:41 +0100

    cards refactoring props

--- src/assets/Card.jsx
+++ src/assets/Card.jsx
@@ -1,20 +1,24 @@
 import Image from "next/image";
 
-function Card({ imageUrl, text, imageName, textContainerClasses }) {
+function Card({ imageUrl, text, imageName}) {
     return (
         <div className="container">
-        <div className="flex flex-col justify-center items-center">
-            {/* Image */}
-            <div>
-                <Image width={350} height={400} src={imageUrl} alt={imageName}></Image>  
-            </div>
-    
-            {/* Card Content */}
-            <div className={`text-container ${textContainerClasses}`}>
-                <p>{text}</p>
+            <div className="flex justify-center items-center mt-10 p-8 h-full">
+                <div className="text-2xl w-[350px] md:w-[550px] h-full border-2 border-yellow-500 p-6">
+                    <div className="flex flex-col justify-center items-center">
+                        {/* Image */}
+                        <div>
+                            <Image width={350} height={400} src={imageUrl} alt={imageName}></Image>  
+                        </div>
+                        {/* Card Content */}
+                        <div className="text-center text-lg md:text-xl overflow-hidden pt-4">
+                            <p>{text}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        </div>
+        
         );
     }
     export default Card;
