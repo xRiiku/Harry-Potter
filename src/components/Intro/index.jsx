66e1af6d8d153831210851a9f16d285da4e5b42d
Daniel Ramon Campos <drc89.sxm@gmail.com>
2024-01-07 13:06:00 +0100

    video moved to public folder

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,18 +1,16 @@
-import video from "@/videos/IntroVideo.mp4"
-
 function Video() {
     return (
         <div className="relative h-[80vh] w-full max-w-[100vw] md:aspect-video md:h-full">
             <video
                 className="h-full w-full animate-fade object-cover object-center animate-duration-1000 animate-ease-in-out"
-                src={video}
+                src="/IntroVideo.mp4"
                 autoPlay
                 loop
                 muted
                 style={{ clipPath: "polygon(0 0, 100% 0, 100% 85%, 50% 100%, 0 85%)" }}
             >
             </video>
-            <video className="absolute -bottom-48 -z-40 blur-3xl w-full" src={video} autoPlay loop muted></video>
+            <video className="absolute -bottom-48 -z-40 blur-3xl w-full" src="/IntroVideo.mp4" autoPlay loop muted></video>
         </div>
     );
 }
