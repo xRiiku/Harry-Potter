26f9dfe5ed9bf862be7c1c84665032a0ba497fcb
Daniel Ramon Campos <drc89.sxm@gmail.com>
2024-01-08 11:57:16 +0100

    characters page + images + selectors

---
+++ src/app/Characters/page.jsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import harry from "@/images/Characters-Selection/harry.webp"
+import dumbledore from "@/images/Characters-Selection/dumbledore.webp"
+import voldemort from "@/images/Characters-Selection/voldemort.webp"
+
+function Characters() {
+    return (
+        <div className="p-10 mt-28 flex justify-center">
+        <div className="flex flex-row gap-3 md:gap-10 p-6">
+            {/* Profesores */}
+            <div>
+                <div className="w-[100px] h-[100px]flex flex-col">
+                    <Image src={dumbledore} alt="Dumbledore"/>
+                </div>
+                    <div className="mt-4 text-center">
+                        <p className="text-xl text-yellow-500">Profesores</p>
+                    </div>
+            </div>
+            {/* Alumnos */}
+            <div>
+                <div className="w-[100px] h-[100px]flex flex-col">
+                    <Image src={harry} alt="Harry"/>
+                </div>
+                    <div className="mt-4 text-center">
+                        <p className="text-xl text-yellow-500">Alumnos</p>
+                    </div>
+            </div>
+            {/* Villanos */}
+            <div>
+                <div className="w-[100px] h-[100px] flex flex-col">
+                    <Image src={voldemort} alt="Voldemort"/>
+                </div>
+                    <div className="mt-4 text-center">
+                        <p className="text-xl text-yellow-500">Villanos</p>
+                    </div>
+            </div>
+        </div>
+        </div>
+    );
+}
+
+export default Characters;
