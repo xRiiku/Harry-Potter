041d2907715d1fa4ff932e597df384dfa51bbd25
RikuDev <riku@rikudev.com>
2024-01-08 18:31:43 +0100

    context character selector

--- src/app/Characters/page.jsx
+++ src/app/Characters/page.jsx
@@ -1,41 +1,54 @@
+"use client"
 import Image from "next/image";
 import harry from "@/images/Characters-Selection/harry.webp"
 import dumbledore from "@/images/Characters-Selection/dumbledore.webp"
 import voldemort from "@/images/Characters-Selection/voldemort.webp"
+import { AppContext } from "@/context/context"
+import { useContext } from "react"
 
 function Characters() {
+
+    const { isSelectorOpen, selectorFunction } = useContext(AppContext);
+
     return (
         <div className="w-screen h-screen p-10 mt-28 flex justify-center">
-        <div className="flex flex-row gap-3 md:gap-10 p-6">
-            {/* Profesores */}
-            <div>
-                <div className="w-[100px] h-[100px]flex flex-col">
-                    <Image src={dumbledore} alt="Dumbledore"/>
-                </div>
-                    <div className="mt-4 text-center">
-                        <p className="text-xl text-yellow-500">Profesores</p>
+            <div className="flex gap-3 md:gap-10 p-6">
+                {/* Profesores */}
+                <div>
+                    <div className="w-[100px] h-[100px]flex flex-col cursor-pointer" onClick={() => selectorFunction(isSelectorOpen)}>
+                        <Image src={dumbledore} alt="Dumbledore"/>
                     </div>
-            </div>
-            {/* Alumnos */}
-            <div>
-                <div className="w-[100px] h-[100px]flex flex-col">
-                    <Image src={harry} alt="Harry"/>
+                        <div className="mt-4 text-center">
+                            <p className="text-xl text-yellow-500">Profesores</p>
+                        </div>
                 </div>
-                    <div className="mt-4 text-center">
-                        <p className="text-xl text-yellow-500">Alumnos</p>
+                {/* Alumnos */}
+                <div>
+                    <div className="w-[100px] h-[100px]flex flex-col">
+                        <Image src={harry} alt="Harry"/>
                     </div>
-            </div>
-            {/* Villanos */}
-            <div>
-                <div className="w-[100px] h-[100px] flex flex-col">
-                    <Image src={voldemort} alt="Voldemort"/>
+                        <div className="mt-4 text-center">
+                            <p className="text-xl text-yellow-500">Alumnos</p>
+                        </div>
                 </div>
-                    <div className="mt-4 text-center">
-                        <p className="text-xl text-yellow-500">Villanos</p>
+                {/* Villanos */}
+                <div>
+                    <div className="w-[100px] h-[100px] flex flex-col">
+                        <Image src={voldemort} alt="Voldemort"/>
                     </div>
+                        <div className="mt-4 text-center">
+                            <p className="text-xl text-yellow-500">Villanos</p>
+                        </div>
+                </div>
+                
+                {isSelectorOpen && (
+                <div>HOLAAAAAA</div>
+                )}
+
             </div>
         </div>
-        </div>
+
+        
     );
 }
 
--- src/context/context.jsx
+++ src/context/context.jsx
@@ -7,15 +7,24 @@ export const AppContext = createContext();
 // Crea el proveedor del contexto
 export default function ContextProvider ({ children }) {
   // Usa useState para crear el estado global
+  //Estado para el menú del navMobile
     const [isMenuOpen, setIsMenuOpen] = useState(false)
-
+    
     function toggleMenu (prev) {
         setIsMenuOpen(!prev)
+    }
+  //Estado para el Selector de personaje (Profesor, alumno, villano)
+    const [isSelectorOpen, setIsSelectorOpen] = useState(false)
+
+    function selectorFunction (prev) {
+        setIsSelectorOpen(!prev)
     }
 
     const contextValue = {
         isMenuOpen,
-        toggleMenu
+        toggleMenu,
+        isSelectorOpen,
+        selectorFunction
     };
 
     return (
